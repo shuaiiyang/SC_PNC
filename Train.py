a1dbc411fb548ad6d28219cbc7736fb6778ff396
Shuai Yang <shuai-y@foxmail.com>
2022-04-04 20:43:10 +0800

    cifar model

--- Train.py
+++ Train.py
@@ -24,22 +24,22 @@ from Model import show_images, PNC_Model
 # 加载图片数据
 def data_loader():
     # Step1: load data cifar100
-    mnist = keras.datasets.mnist
-    (x_train, _), (x_test, _) = mnist.load_data()
-    # cifar = keras.datasets.cifar100
-    # (x_train, _), (x_test, _) = cifar.load_data(label_mode='coarse')
+    # mnist = keras.datasets.mnist
+    # (x_train, _), (x_test, _) = mnist.load_data()
+    cifar = keras.datasets.cifar100
+    (x_train, _), (x_test, _) = cifar.load_data(label_mode='coarse')
     # x_train = tf.image.resize(images=x_train, size=(28, 28))
     # x_test = tf.image.resize(images=x_test, size=(28, 28))
-    # x_train = tf.image.rgb_to_grayscale(x_train)
-    # x_test = tf.image.rgb_to_grayscale(x_test)
-    # x_train = np.array(x_train)
-    # x_test = np.array(x_test)
+    x_train = tf.image.rgb_to_grayscale(x_train)
+    x_test = tf.image.rgb_to_grayscale(x_test)
+    x_train = np.array(x_train)
+    x_test = np.array(x_test)
 
     # Step2: normalize
     x_train = x_train.astype('float32') / 255.
     x_test = x_test.astype('float32') / 255.
-    x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
-    x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
+    # x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+    # x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
     x_train_A = copy.deepcopy(x_train)
     x_train_B = copy.deepcopy(x_train)
     x_test_A = copy.deepcopy(x_test)
@@ -60,24 +60,24 @@ def main():
     # load data
     (x_train_A, x_train_B), (x_test_A, x_test_B) = data_loader()
     # load model
-    # pnc_model = PNC_Model(input_shape=(28, 28, 1))
-    pnc_model = keras.models.load_model('../SE_PNC/Models/SE_model.h5',
-                                       {'ChannelLayerRelay': ChannelLayerRelay,
-                                        'ChannelLayer': ChannelLayer,
-                                        'ResidualBlockTx': ResidualBlockTx,
-                                        'ResidualBlock': ResidualBlock,
-                                        'ResidualBlockRx': ResidualBlockRx,
-                                        'PeakSignalToNoiseRatio': PeakSignalToNoiseRatio,
-                                        'TxModel': TxModel,
-                                        'TxRModel': TxRModel,
-                                        'RxModel': RxModel
-                                        })
-    # pnc_model.summary()
+    pnc_model = PNC_Model(input_shape=(32, 32, 1))
+    # pnc_model = keras.models.load_model('../SE_PNC/Models/SE_model.h5',
+    #                                    {'ChannelLayerRelay': ChannelLayerRelay,
+    #                                     'ChannelLayer': ChannelLayer,
+    #                                     'ResidualBlockTx': ResidualBlockTx,
+    #                                     'ResidualBlock': ResidualBlock,
+    #                                     'ResidualBlockRx': ResidualBlockRx,
+    #                                     'PeakSignalToNoiseRatio': PeakSignalToNoiseRatio,
+    #                                     'TxModel': TxModel,
+    #                                     'TxRModel': TxRModel,
+    #                                     'RxModel': RxModel
+    #                                     })
+    pnc_model.summary()
 
     # Step3: train
     epochs = 5
     # save model graph
-    keras.utils.plot_model(pnc_model, to_file='../SE_PNC/Models/SE_model.png', show_shapes=True)
+    keras.utils.plot_model(pnc_model, to_file='../SE_PNC/Models/SE_model_cifar.png', show_shapes=True)
     pnc_model.fit(x=[x_train_A, x_train_B],
                   y=[x_train_A, x_train_B],
                   batch_size=100,
@@ -85,7 +85,7 @@ def main():
                   validation_data=([x_test_A, x_test_B], [x_test_A, x_test_B]),
                   shuffle=True
                   )
-    pnc_model.save("../SE_PNC/Models/SE_model.h5")
+    pnc_model.save("../SE_PNC/Models/SE_model_cifar.h5")
 
 
 if __name__ == '__main__':
